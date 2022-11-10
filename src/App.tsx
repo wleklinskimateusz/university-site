043f8d6b5d2c3b5384194d080aa8f46c5438e252
wleklinskimateusz <wleklinskimateusz05@gmail.com>
2022-11-10 15:51:27 +0100

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  AppstoreOutlined,
+  MailOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import type { MenuProps } from "antd";
+import { Menu } from "antd";
+import React, { useState } from "react";
+import useDarkMode, { DarkModeToggler } from "use-dark-mode-hook";
 
-function App() {
+const items: MenuProps["items"] = [
+  {
+    label: "Navigation One",
+    key: "mail",
+    icon: <MailOutlined />,
+  },
+  {
+    label: "Navigation Two",
+    key: "app",
+    icon: <AppstoreOutlined />,
+  },
+  {
+    label: "Navigation Three - Submenu",
+    key: "SubMenu",
+    icon: <SettingOutlined />,
+    children: [
+      {
+        type: "group",
+        label: "Item 1",
+        children: [
+          {
+            label: "Option 1",
+            key: "setting:1",
+          },
+          {
+            label: "Option 2",
+            key: "setting:2",
+          },
+        ],
+      },
+      {
+        type: "group",
+        label: "Item 2",
+        children: [
+          {
+            label: "Option 3",
+            key: "setting:3",
+          },
+          {
+            label: "Option 4",
+            key: "setting:4",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    label: (
+      <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+        Navigation Four - Link
+      </a>
+    ),
+    key: "alipay",
+  },
+];
+
+const App: React.FC = () => {
+  const [isDarkMode, toggleDarkMode] = useDarkMode({});
+  const [current, setCurrent] = useState("mail");
+
+  const onClick: MenuProps["onClick"] = (e) => {
+    console.log("click ", e);
+    setCurrent(e.key);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Menu
+        onClick={onClick}
+        selectedKeys={[current]}
+        mode="horizontal"
+        items={items}
+        theme={isDarkMode ? "dark" : "light"}
+      />
+      <div style={{ position: "absolute", top: "5px", right: "5px" }}>
+        <DarkModeToggler
+          isDarkMode={isDarkMode}
+          toggleDarkMode={toggleDarkMode}
+        />
+      </div>
+    </>
   );
-}
+};
 
 export default App;
